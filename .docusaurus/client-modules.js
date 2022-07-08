0d40809208dda4030aae8444024739f278584c7d
Reich <nreich@bcd074120c77.ant.amazon.com>
2022-07-08 14:14:42 -0700

    First test build!!

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/nreich/workshop/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/nreich/workshop/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/nreich/workshop/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/nreich/workshop/src/css/custom.css'),
+];
